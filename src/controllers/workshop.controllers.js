b2f683baed9d0065c3d6bd78ada765443ef8481d
Fernando <fer3443@gmail.com>
2024-08-13 14:37:43 -0300

    feat: nuevos controladores añadidos, virtualDeleteWorkshop y deleteWorkshop

--- src/controllers/workshop.controllers.js
+++ src/controllers/workshop.controllers.js
@@ -3,9 +3,25 @@ const Workshop = require("../models/workshop.model");
 const addWorkshop = async (req, res) => {
   try {
     const { title, description, speaker, date, image } = req.body;
-		if(!title.length || !description.length || !speaker.length || !date.length || !image.length) return res.status(500).json({message: 'Todos los campos deben completarse'});
-    const newWorkshop = new Workshop({title, description, speaker, date, image});
-    if(!newWorkshop) return res.status(400).json({message:"no se pudo crear el workshop"})
+    if (
+      !title.length ||
+      !description.length ||
+      !speaker.length ||
+      !date.length ||
+      !image.length
+    )
+      return res
+        .status(500)
+        .json({ message: "Todos los campos deben completarse" });
+    const newWorkshop = new Workshop({
+      title,
+      description,
+      speaker,
+      date,
+      image,
+    });
+    if (!newWorkshop)
+      return res.status(400).json({ message: "no se pudo crear el workshop" });
     const workshopCreated = await newWorkshop.save();
     return res
       .status(200)
@@ -17,50 +33,103 @@ const addWorkshop = async (req, res) => {
   }
 };
 
-const updateWorkshop = async (req,res) => {
+const updateWorkshop = async (req, res) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const data = req.body;
-    const updatedWorkshop = await Workshop.findByIdAndUpdate(id, data, {new: true});
-    if(!updatedWorkshop){
-      return res.status(404).json({message: "Workshop no encontrado"})
+    const updatedWorkshop = await Workshop.findByIdAndUpdate(id, data, {
+      new: true,
+    });
+    if (!updatedWorkshop) {
+      return res.status(404).json({ message: "Workshop no encontrado" });
     }
-    return res.status(200).json({message:"Workshop actualizado con éxito", data: updatedWorkshop})
+    return res
+      .status(200)
+      .json({
+        message: "Workshop actualizado con éxito",
+        data: updatedWorkshop,
+      });
   } catch (error) {
-    return res.status(500).json({message:`Hubo un error al actualizar datos: ${error}`})
+    return res
+      .status(500)
+      .json({ message: `Hubo un error al actualizar datos: ${error}` });
   }
-}
+};
 
 const getWorkshop = async (req, res) => {
   try {
-    const {id} = req.params;
-    const workshop = await Workshop.findById(id)
-    if(!workshop){
-      return res.status(404).json({message:"Workshop no encontrado"})
+    const { id } = req.params;
+    const workshop = await Workshop.findById(id);
+    if (!workshop) {
+      return res.status(404).json({ message: "Workshop no encontrado" });
     }
-    return res.status(200).json({message:"Petición exitosa", data: workshop})
+    return res
+      .status(200)
+      .json({ message: "Petición exitosa", data: workshop });
   } catch (error) {
-    return res.status(500).json({message:`Ha ocurrido un error: ${error}`})
+    return res.status(500).json({ message: `Ha ocurrido un error: ${error}` });
   }
-}
+};
 
 const getAllWorkshops = async (req, res) => {
-  const maxElements = 10
+  const maxElements = 10;
   try {
     const page = parseInt(req.query.page) || 1;
-    const workshops = await Workshop.find({}).skip(maxElements * (page - 1))
-    .limit(maxElements);
-    if(!workshops.length){
-      return res.status(404).json({message:"No hay workshops disponibles"})
+    const workshops = await Workshop.find({})
+      .skip(maxElements * (page - 1))
+      .limit(maxElements);
+    if (!workshops.length) {
+      return res.status(404).json({ message: "No hay workshops disponibles" });
     }
-    return res.status(200).json({message:"Peticion exitosa", data: workshops})
+    return res
+      .status(200)
+      .json({ message: "Peticion exitosa", data: workshops });
   } catch (error) {
-    return res.status(500).json({message:`Hubo un error: ${error}`})
+    return res.status(500).json({ message: `Hubo un error: ${error}` });
   }
-}
+};
+
+const virtualDeleteWorkshop = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const vDeleteWorkshop = await Workshop.findByIdAndUpdate(
+      id,
+      { virtualDelete: true },
+      { new: true }
+    );
+    if (!vDeleteWorkshop)
+      return res.status(404).json({ message: "No se encontro el workshop" });
+    return res
+      .status(200)
+      .json({
+        message: "El workshop ha sido eliminado correctamente",
+        data: vDeleteWorkshop,
+      });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: `Ha ocurrido un error al eliminar: ${error}` });
+  }
+};
+
+const deleteWorkshop = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteWorkshop = await Workshop.findByIdAndDelete(id);
+    if (!deleteWorkshop)
+      return res.status(404).json({ message: "No ha encontrado el workshop" });
+    return res
+      .status(200)
+      .json({ message: "Workshop eliminado con exito", data: deleteWorkshop });
+  } catch (error) {
+    return res.status(500).json({ message: `Ha ocurrido un error: ${error}` });
+  }
+};
 module.exports = {
   addWorkshop,
   updateWorkshop,
   getWorkshop,
-  getAllWorkshops
+  getAllWorkshops,
+  virtualDeleteWorkshop,
+  deleteWorkshop,
 };
--- src/routes/workshop.routes.js
+++ src/routes/workshop.routes.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 const route = Router();
-const { addWorkshop, updateWorkshop, getWorkshop, getAllWorkshops } = require("../controllers/workshop.controllers");
+const { addWorkshop, updateWorkshop, getWorkshop, getAllWorkshops, virtualDeleteWorkshop, deleteWorkshop } = require("../controllers/workshop.controllers");
 const { isAdmin, isAuthenticated } = require("../middlewares/auth");
 
 route.post("/create-workshop", isAuthenticated, isAdmin("admin"), addWorkshop);
 route.put("/update-workshop/:id", isAuthenticated, isAdmin("admin"), updateWorkshop);
 route.get("/get-workshop/:id", isAuthenticated, getWorkshop);
+route.put("/v-delete-workshop/:id", isAuthenticated, isAdmin("admin"), virtualDeleteWorkshop);
+route.delete("/delete-workshop/:id", isAuthenticated, isAdmin("admin"), deleteWorkshop);
 route.get("/", getAllWorkshops)
 
 module.exports = route
